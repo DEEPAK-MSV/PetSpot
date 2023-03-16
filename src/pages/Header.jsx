4e3dece1cbe537948da2ad0b0722ba2ce7e7a8af
DEEPAK-MSV <deepak@getpulse.in>
2023-03-16 14:53:22 +0530

    .

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom"
-// import Dogs from "../Animals/Dogs"
-// import Cats from "../Animals/Cats"
 
 function Header() {
     return (
